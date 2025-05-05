971bdad2b546727fdb80999238e8aca217b889f3
Mayank <mayankn3173@gmail.com>
2025-05-05 23:26:14 +0530

    Time: 0 ms (100%), Space: 8.7 MB (43.48%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     bool isPalindrome(int x) {
         if(x<0) return false;
-        if(x<10) return true;
         long long num=0,n=x;
         while(n>0){
             num=num*10+n%10;
