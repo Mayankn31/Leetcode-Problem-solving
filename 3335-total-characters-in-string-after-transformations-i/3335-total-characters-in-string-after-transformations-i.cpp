0bac9c39e78bb80732999356554bbfb73365e055
Mayank <mayankn3173@gmail.com>
2025-05-14 03:11:22 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3335-total-characters-in-string-after-transformations-i/3335-total-characters-in-string-after-transformations-i.cpp
+++ 3335-total-characters-in-string-after-transformations-i/3335-total-characters-in-string-after-transformations-i.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     int lengthAfterTransformations(string s, int t) {
-        int mod=1e9+7;
+        const int MOD=1000000007;
         vector<int>fre(26,0);
-        for(char c:s) fre[c-'a']++;
+        for(char x:s) fre[x-'a']++;
         while(t--){
             vector<int>temp(26,0);
             if(fre[25]>0){
-                temp[0]=(fre[25])%mod;
-                temp[1]=(fre[25])%mod;
+                temp[0]=(fre[25])%MOD;
+                temp[1]=(fre[0]+fre[25])%MOD;
             }
-            for(int i=1;i<26;i++){
-                temp[i]=(temp[i]+fre[i-1])%mod;
+            else temp[1]=fre[0];
+            for(int i=2;i<26;i++){
+                temp[i]+=(fre[i-1])%MOD;
             }
             fre=temp;
         }
         int res=0;
-        for(int x:fre) res=(res+x)%mod;
+        for(int x:fre) res+=(x)%MOD;
         return res;
     }
 };
