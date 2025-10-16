a25ce70061838de40988d95c631ddea279486669
Mayank <mayankn3173@gmail.com>
2025-10-17 03:20:27 +0530

    Time: 72 ms (42.68%), Space: 135.7 MB (43.5%) - LeetHub

--- 2598-smallest-missing-non-negative-integer-after-operations/2598-smallest-missing-non-negative-integer-after-operations.cpp
+++ 2598-smallest-missing-non-negative-integer-after-operations/2598-smallest-missing-non-negative-integer-after-operations.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
     int findSmallestInteger(vector<int>& nums, int value) {
-        int n=nums.size();
-        unordered_set<int>s;
         unordered_map<int,int>mp;
-        for(int x:nums){
+        for(int &x:nums){
             int cur=x%value;
             if(cur<0) cur+=value;
-            int newCur=cur;
-            if(mp.count(cur)) newCur=cur+value*mp[cur];
             mp[cur]++;
-            s.insert(newCur);
         }
-        for(int i=0;i<n;i++){
-            if(s.count(i)) continue;
-            else return i;
+        int MEX=0;
+        while(mp[MEX%value]>0){
+            mp[MEX%value]--;
+            MEX++;
         }
-        return n;
+        return MEX;
     }
 };
