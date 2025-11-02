74ed23e4040e3fe4cffaf90f9fcd57387b12118b
Mayank <mayankn3173@gmail.com>
2025-11-03 01:09:17 +0530

    Time: 48 ms (87.14%), Space: 173.2 MB (49.05%) - LeetHub

--- 2257-count-unguarded-cells-in-the-grid/2257-count-unguarded-cells-in-the-grid.cpp
+++ 2257-count-unguarded-cells-in-the-grid/2257-count-unguarded-cells-in-the-grid.cpp
@@ -31,7 +31,6 @@ public:
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]==1){
-                    grid[i][j]=3;
                     dfs(i,j,grid);
                 }
             }
