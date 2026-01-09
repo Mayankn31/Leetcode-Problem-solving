9171a350c2659a2f26d0ad07ca45e5ec8203113d
Mayank <mayankn3173@gmail.com>
2026-01-10 01:04:05 +0530

    Time: 1 ms (5.52%), Space: 8.4 MB (50.72%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int func(int i,vector<int>& dp){
+        if(i<0) return 0;
+        if(i==0) return 1;
+        if(dp[i]!=-1) return dp[i];
+        return dp[i]=func(i-1,dp)+func(i-2,dp);
+    }
+    int climbStairs(int n) {
+        vector<int> dp(n+1,-1);
+        return func(n,dp);
+    }
+};
